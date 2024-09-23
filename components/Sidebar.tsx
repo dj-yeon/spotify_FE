3d1e1485f52aef13edeb0394b135505d66bf9f8a
Dongju Yeon <dongju1497@gmail.com>
2024-09-23 16:59:28 +0900

    feat: added sample login data

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -55,6 +55,31 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
         <Box className="overflow-y-auto h-full">
           <Library songs={songs} />
         </Box>
+        <Box className="overflow-y-auto h-[25%]">
+          <div className="text-white px-5 py-4 text-lg space-y-4">
+            {/* USER Section */}
+            <div>
+              <h3 className="font-bold text-xl mb-2">USER Sample Account</h3>
+              <p>
+                <span className="font-semibold">ID:</span> user@dongju.link
+              </p>
+              <p>
+                <span className="font-semibold">Password:</span> user
+              </p>
+            </div>
+
+            {/* ADMIN Section */}
+            <div>
+              <h3 className="font-bold text-xl mb-2">ADMIN Sample Account</h3>
+              <p>
+                <span className="font-semibold">ID:</span> admin@dongju.link
+              </p>
+              <p>
+                <span className="font-semibold"> Password:</span> admin
+              </p>
+            </div>
+          </div>
+        </Box>
       </div>
       <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
